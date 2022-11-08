caf78d02965f9cf496e29c74a293cecaac5ba546
Ankush Thakur <ankusht2307@gmail.com>
2022-11-09 00:45:52 +0530

    Added problem in Readme and formatting

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 - [Excel Sheet Column Title](https://github.com/ankusht2307/leetcode-problems/blob/main/String/Easy/168/index.js)
 - [Valid Palindrome](https://github.com/ankusht2307/leetcode-problems/blob/main/String/Easy/125/index.js)
 - [Reverse Words in a String III](https://github.com/ankusht2307/leetcode-problems/blob/main/String/Easy/557/index.js)
+- [ Robot Return to Origin](https://github.com/ankusht2307/leetcode-problems/blob/main/String/Easy/657/index.js)
 
 
 ## Authors
--- String/Easy/657/index.js
+++ String/Easy/657/index.js
@@ -5,34 +5,33 @@
  * @param {string} moves
  * @return {boolean}
  */
- var judgeCircle = function(moves) {
-    let moveX = 0;
-    let moveY = 0;
-    
-    
-    for (let i = 0; i < moves.length; i++) {
-        switch (moves[i]) {
-            case 'R':
-                moveX++;
-                break;
-            case 'L':
-                moveX--;
-                break;
-            case 'U':
-                moveY++;
-                break;
-            case 'D':
-                moveY--;
-                break;
-                
-        }
-    }
-    
-    return moveX === 0 && moveY === 0;
-}
+var judgeCircle = function (moves) {
+  let moveX = 0;
+  let moveY = 0;
 
-const moves = "UD";
+  for (let i = 0; i < moves.length; i++) {
+    switch (moves[i]) {
+      case 'R':
+        moveX++;
+        break;
+      case 'L':
+        moveX--;
+        break;
+      case 'U':
+        moveY++;
+        break;
+      case 'D':
+        moveY--;
+        break;
+    }
+  }
+
+  return moveX === 0 && moveY === 0;
+};
+
+const moves = 'UD';
 const result = judgeCircle(moves);
 console.log(result);
 
 // time complexity: O(n)
+// space complexity: O(n)

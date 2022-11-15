58fb9e01de81af76af32c26e6bd2eb9f2a22b62b
Ankush Thakur <ankusht2307@gmail.com>
2022-11-16 02:30:37 +0530

    Factorial

--- MathAlgorithms/Fibonacci/index.js
+++ MathAlgorithms/Fibonacci/index.js
@@ -1,3 +1,14 @@
+/**
+ * Problem: Given a number 'n', find the first 'n' elements
+ * of the Fibonacci sequence.
+ *
+ * In Mathematics, the fibonacci sequence is a sequence in which
+ * each number is the sum of the two preceding ones.
+ *
+ * @param {*} n
+ * @returns []
+ */
+
 const fibonacci = (n) => {
   let fib = [0, 1];
   for (let i = 2; i < n; i++) {
@@ -14,4 +25,4 @@ console.log(fibonacci(7));
 console.log(fibonacci(9999999));
 
 // Big-O: O(n) - Linear based on the input.
-// Space Complexity: O(n) - Also linear because size of array grows with input
+// Space Complexity: O(n) - Also linear because size of array grows with input.
